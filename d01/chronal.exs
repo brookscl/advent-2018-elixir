98dfa593d26228faf0977138f7b9703652801fa8
Chris Brooks <chris@chrisbrooks.org>
2018-12-03 22:08:24 -0800

    Non-recursive, enum stream only answer

--- d01/chronal.exs
+++ d01/chronal.exs
@@ -1,14 +1,7 @@
-defmodule Mathy do
-	def sum_list(list), do: do_sum_list(list, 0)
-
-	defp do_sum_list([], acc),		do: acc
-	defp do_sum_list([h|t], acc), do: do_sum_list(t, acc + h)
-end
-
-numbers =
+result =
 	File.stream!("input.txt")
 	|> Stream.map(&String.strip/1)
 	|> Stream.map(&(String.to_integer(&1)))
-	|> Enum.to_list
+	|> Enum.sum
 
-IO.inspect Mathy.sum_list(numbers)
+IO.inspect result
